1f2abaec71a8f36d3f153449c1a1b2bccc75fd03
AlanCordova <Khambastram@yahoo.com.br>
2021-10-15 20:03:28 -0300

    avanços no curso

---
+++ fundamentos-js/usandoLet1.js
@@ -0,0 +1,12 @@
+var numero = 1
+{
+    let numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+
+//var tem escopo global e escopo de funcao
+//let tem escopo global, funcao e bloco
+
+//escopo = uma 'variável' definida exclusivamente dentro da função não 
+//pode ser acessada de fora da função ou dentro de outras funções
