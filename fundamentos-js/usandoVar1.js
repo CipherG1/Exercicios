32af524549ab3c8eb402e87549c3418982e59450
AlanCordova <Khambastram@yahoo.com.br>
2021-10-14 21:40:11 -0300

    progredindo no curso

---
+++ fundamentos-js/usandoVar1.js
@@ -0,0 +1,22 @@
+{
+    {
+        {
+            {
+                 var sera = 'Será???'
+            }
+        }
+    }
+}
+console.log(sera)
+
+function teste() {
+    var local = 123
+}
+
+teste()
+//console.log(local) causa erro ao tentar buscar algo que nao esta
+//dentro do escopo de funcao
+//*Escopo de function é 
+// function teste() {
+// var local = 123
+// } 
