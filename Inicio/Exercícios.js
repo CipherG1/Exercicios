6e926acb919db183f6adfef6dc8f397ec0646fa4
AlanCordova <Khambastram@yahoo.com.br>
2022-03-21 21:20:49 -0300

    atualizar

--- Inicio/Exercícios.js
+++ Inicio/Exercícios.js
@@ -60,8 +60,303 @@ console.log(jurosCompostos(100, 10/100, 2));
 
 
 
-
-
-
-
 //7)
+/*
+function bhaskara (ax2, bx, c) {
+    let resultados = []
+    let delta = (bx ** 2) - (4 * ax2 * c)
+    if(delta < 0){
+        return "Delta é negativo"
+    } 
+    let x1 = (-bx + Math.sqrt(delta))/(2*ax2)
+    let x2 = (-bx - Math.sqrt(delta))/(2*ax2)
+    resultados.push(x1)
+    resultados.push(x2)
+    return resultados   
+}
+
+console.log(bhaskara(1, 3, 2))
+console.log(bhaskara(3, 1, 2))
+*/
+
+
+//8)
+/*
+let stringPontuacoes = "30, 40, 20, 4, 51, 25, 42, 38, 56, 0"
+ 
+ 
+function avaliaPontuacoes (stringPontuacoes) {
+    let pontuacoes = stringPontuacoes.split(", ")
+    let qtdQuebraDeRecords = 0
+    let piorJogo = 1
+    let maiorPontuacao = pontuacoes[0]
+    let menorPontuacao = pontuacoes[0]
+
+    for (let i = 1; i < pontuacoes.length; i++) {
+        if(pontuacoes[i] > maiorPontuacao) {
+            maiorPontuacao = pontuacoes[i]
+            qtdQuebraDeRecords++
+        }else if (pontuacoes[i] < menorPontuacao) {
+            menorPontuacao = pontuacoes[i]
+            piorJogo = i+1;
+        }
+    }
+    return [qtdQuebraDeRecords, piorJogo]
+}
+ 
+console.log(avaliaPontuacoes(stringPontuacoes))
+*/
+
+
+//9)
+/*
+function classificaAluno(nota) {
+    let notaCorrigida = arredondar(nota)
+    if (notaCorrigida >= 40) {
+        console.log(`Aprovado com nota ${notaCorrigida}`);
+    } else {
+        console.log(`Reprovado com nota ${notaCorrigida}`);
+    }
+}
+
+function arredondar (nota) {
+    if (nota % 5 > 2) {
+        return nota + (5 - (nota % 5))
+    } else {
+        return nota
+    }
+}
+
+classificaAluno(100)
+classificaAluno(30)
+classificaAluno(38)
+classificaAluno(88)
+classificaAluno(61)
+*/
+
+//10)
+/*
+function verificaNumero (numero) {
+    if(numero % 3 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(verificaNumero(3))
+console.log(verificaNumero(2))
+console.log(verificaNumero(150))
+*/
+
+
+//11) *Cálculo de ano bissexto*
+/*
+function calcularAnoBissexto (ano) {
+    if (ano <= 0){
+        return false
+    } else if (ano % 400 == 0) {
+        return true
+    } else if (ano % 100 == 0) {
+        return false
+    } else if (ano % 4 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(calcularAnoBissexto(0))
+console.log(calcularAnoBissexto(4))
+console.log(calcularAnoBissexto(100))
+console.log(calcularAnoBissexto(400))
+console.log(calcularAnoBissexto(800))
+console.log(calcularAnoBissexto(2020))
+console.log(calcularAnoBissexto(2021))
+console.log(calcularAnoBissexto(2028))
+*/
+
+//12)
+/*function fatorial (numero) {
+    if(numero == 0){
+        return 1
+    } else {
+        return numero * fatorial(numero - 1)
+    }
+}
+
+console.log(fatorial(10))
+*/
+
+//13)
+/*function classificaDia(numero) {
+    switch (numero) {
+        case 1:
+            return "Fim de semana"
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6: 
+            return "Dia útil"
+        case 7:
+            return "Fim de semana"
+        default: 
+            return "Dia inválido"
+    }
+}
+
+console.log(classificaDia(1));
+console.log(classificaDia(2));
+console.log(classificaDia(3));
+console.log(classificaDia(4));
+console.log(classificaDia(5));
+console.log(classificaDia(6));
+console.log(classificaDia(7));
+console.log(classificaDia('a'));
+*/
+
+//14)
+/*function venderFruta (fruta) {
+    switch (fruta) {
+        case 'maçã':
+            console.log('Não vendemos esta fruta aqui.')
+            break
+        case 'kiwi':
+            console.log('Estamos com escassez de kiwi.')
+            break
+        case 'melancia':
+            console.log('Aqui está, custa R$ 3,00 o quilo.')
+            break
+        default:
+            console.log('Erro, fruta inválida.')
+    }
+}
+
+venderFruta('maçã')
+venderFruta('kiwi')
+venderFruta('melancia')
+venderFruta('alcatra')
+*/
+
+//15)
+/*
+function escolherCarro (modelo) {
+    switch (modelo) {
+        case 'hatch':
+            return 'Compra efetuada com sucesso.'
+        case 'sedan':
+        case 'motocicleta':
+        case 'caminhonete':
+            return 'Tem certeza que não prefere este modelo?'
+        default:
+            return 'Não trabalhamos com este tipo de automóvel aqui.'
+    }
+}
+
+console.log(escolherCarro('hatch'));
+console.log(escolherCarro('motocicleta'));
+console.log(escolherCarro('sedan'));
+console.log(escolherCarro('caminhonete'));
+console.log(escolherCarro('jetski'));
+*/
+
+//16)
+/*
+function calculadora (valor1, operacao, valor2) {
+    switch (operacao) {
+        case '+':
+            return valor1 + valor2
+        case '-':
+            return valor1 - valor2
+        case '*':
+            return valor1 * valor2
+        case '/':
+            return valor1 / valor2
+        default:
+            return 'Operação inválida'
+    }
+}
+
+console.log(calculadora(2, '+', 3));
+console.log(calculadora(2, '-', 3));
+console.log(calculadora(2, '*', 3));
+console.log(calculadora(2, '/', 3));
+console.log(calculadora(2, 'a', 3));
+*/
+
+//17)
+/*function calcularReajuste(plano, salarioAtual) {
+    switch (plano) {
+        case 'A':
+            return salarioAtual * 1.1
+        case 'B':
+            return salarioAtual * 1.15
+        case 'C':
+            return salarioAtual * 1.2
+        default:
+            return 'Plano inválido.'
+    }
+}
+
+console.log(calcularReajuste('A', 1000));
+console.log(calcularReajuste('B', 1000));
+console.log(calcularReajuste('C', 1000));
+console.log(calcularReajuste('D', 1000));
+*/
+
+//18)
+/*
+function numeroPorExtenso(numero) {
+    switch (numero) {
+        case 0:
+            console.log('Zero')
+            break
+        case 1:
+            console.log('Um')
+            break
+        case 2:
+            console.log('Dois')
+            break
+        case 3:
+            console.log('Três')
+            break
+        case 4:
+            console.log('Quatro')
+            break
+        case 5:
+            console.log('Cinco')
+            break
+        case 6:
+            console.log('Seis')
+            break
+        case 7:
+            console.log('Sete')
+            break
+        case 8:
+            console.log('Oito')
+            break
+        case 9:
+            console.log('Nove')
+            break
+        case 10:
+            console.log('Dez')
+            break
+        default:
+            console.log('Numero fora do intervalo.')
+    }
+}
+
+numeroPorExtenso(0)
+numeroPorExtenso(1)
+numeroPorExtenso(2)
+numeroPorExtenso(3)
+numeroPorExtenso(4)
+numeroPorExtenso(5)
+numeroPorExtenso(6)
+numeroPorExtenso(7)
+numeroPorExtenso(8)
+numeroPorExtenso(9)
+numeroPorExtenso(10)
+numeroPorExtenso(11)
+*/
