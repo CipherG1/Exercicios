1bbdc6a45ad940a8bc7fd5150827eb1689086caa
AlanCordova <Khambastram@yahoo.com.br>
2022-04-14 02:07:36 -0300

    map2completo

--- array/map2.js
+++ array/map2.js
@@ -4,3 +4,11 @@ const carrinho = [
     '{"nome": "Kit de Lapis", "preco": 41.22 }',
     '{"nome": "Caneta", "preco": 7.50 }'
 ]
+
+// Retornar um array apenas com os preços
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
